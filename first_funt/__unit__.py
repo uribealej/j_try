2e992d6e5914ddad02f83a6e863a29c02ff1d2d5
uribealej <uribealej@gmail.com>
2024-07-17 16:57:20 +0200

    momno

---
+++ first_funt/__unit__.py
@@ -0,0 +1,6 @@
+# first_funt.py
+
+x = 42  # Define the attribute x
+
+def first_funt():
+    print("This is an example function.")
---
+++ first_funt/data.py
@@ -0,0 +1,6 @@
+# first_funt.py
+
+x = 42  # Define the attribute x
+
+def example_function():
+    print("This is an example function.")
